5bf9ff0f0f55fda5071a105766b017e69ef245c4
purple <904839562@qq.com>
2023-02-02 13:38:32 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/assessment/gson/Suggestion.java
+++ app/src/main/java/com/example/assessment/gson/Suggestion.java
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName;
 public class Suggestion {
 
     @SerializedName("comf")
-    private Comfort comfort;
+    public Comfort comfort;
 
     @SerializedName("cw")
     public CarWash carWash;
